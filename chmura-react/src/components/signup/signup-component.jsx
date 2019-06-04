77de233e82e924f310798e700215c064cf8e7d05
Tomek Zalewski <11194646+tomzal97@users.noreply.github.com>
2019-06-04 19:09:46 +0200

    something

--- chmura-react/src/components/signup/signup-component.jsx
+++ chmura-react/src/components/signup/signup-component.jsx
@@ -8,27 +8,8 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Container from '@material-ui/core/Container';
-import {ReactMic} from "react-mic";
 
 export function Signup() {
-    let record = false;
-
-    let startRecording = () => {
-        record = true;
-    }
-
-    let stopRecording = () => {
-        record = false;
-    }
-
-    let onData = (recordedBlob) => {
-        console.log('chunk of real-time data is: ', recordedBlob);
-    }
-
-    let onStop = (recordedBlob) => {
-        console.log('recordedBlob is: ', recordedBlob);
-    }
-
     return (
         <Container maxWidth="md">
             <Card>
@@ -38,15 +19,6 @@ export function Signup() {
                         margin="normal"
                         variant="outlined" />
                     <Typography variant="body2" component="p">Your phrase:  <b>"Houston we have had a problem"</b></Typography>
-                    <ReactMic
-                        record={this.record}
-                        className="sound-wave"
-                        onStop={this.onStop}
-                        onData={this.onData}
-                        strokeColor="#000000"
-                        backgroundColor="#FF4081" />
-                    <button onTouchTap={this.startRecording} type="button">Start</button>
-                    <button onTouchTap={this.stopRecording} type="button">Stop</button>
                 </CardContent>
                 <CardActions>
                     <Button variant="contained" color="primary">Sign in</Button>
