013ad0a41968a63ff686dee5ed13e0668a210293
Karol Ulanowski <ulanowskikarol@gmail.com>
2019-06-08 00:13:03 +0200

    s3 serivice

---
+++ GugylDrajfApi/GugylDrajfApi/Services/S3Service.cs
@@ -0,0 +1,58 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using GugylDrajfApi.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GugylDrajfApi.Services
+{
+    public class S3Service:IS3Service
+    {
+        //TODO: move to settings
+        private readonly string bucketName = "gugyldrajf-files";
+        private readonly IAmazonS3 _client;
+
+        public S3Service(IAmazonS3 client)
+        {
+            _client = client;
+        }
+
+        public async Task<S3Response> UploadFileToS3(string azureId,IFormFile file)
+        {
+            try
+            {
+                using (var newMemoryStream = new MemoryStream())
+                {
+                    file.CopyTo(newMemoryStream);
+
+                    var uploadRequest = new TransferUtilityUploadRequest
+                    {
+                        InputStream = newMemoryStream,
+                        Key = $"{azureId}/{file.FileName}",
+                        BucketName = bucketName,
+                        CannedACL = S3CannedACL.Private
+                    };
+
+                    var fileTransferUtility = new TransferUtility(_client);
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+                return new S3Response(HttpStatusCode.OK);
+            }
+            catch(AmazonS3Exception e)
+            {
+                return new S3Response(e.StatusCode,e.Message);
+            }
+            catch (Exception e)
+            {
+                return new S3Response(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+    }
+}
--- GugylDrajfApi/GugylDrajfApi/Startup.cs
+++ GugylDrajfApi/GugylDrajfApi/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.Extensions.NETCore.Setup;
+using Amazon.S3;
 using GugylDrajfApi.Repositories;
+using GugylDrajfApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,13 +34,15 @@ namespace GugylDrajfApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddAWSService<IAmazonDynamoDB>();
+            services.AddAWSService<IAmazonS3>();
             services.AddDefaultAWSOptions(
                 new AWSOptions
                 {
                     Region = RegionEndpoint.GetBySystemName("eu-central-1")
                 });
 
-            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddSingleton<IS3Service, S3Service>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
