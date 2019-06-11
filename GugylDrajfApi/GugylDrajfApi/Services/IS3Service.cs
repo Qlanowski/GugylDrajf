c2cf15648e0ae2680e74c8e5e5b84043ff9aea40
Tomek Zalewski <11194646+tomzal97@users.noreply.github.com>
2019-06-12 01:34:32 +0200

    Add delete api

--- GugylDrajfApi/GugylDrajfApi/Services/IS3Service.cs
+++ GugylDrajfApi/GugylDrajfApi/Services/IS3Service.cs
@@ -1,4 +1,5 @@
-﻿using GugylDrajfApi.Models;
+﻿using Amazon.S3.Model;
+using GugylDrajfApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -14,5 +15,7 @@ namespace GugylDrajfApi.Services
         Task<S3Response> UploadFileToS3(string azureId, IFormFile file);
         string GetDownloadFileUrl(string azureId, string filename);
         Task<IEnumerable<FileMetadata>> FileNames(string azureId);
+
+        Task<DeleteObjectResponse> DeleteFile(string azureId, string filename);
     }
 }
